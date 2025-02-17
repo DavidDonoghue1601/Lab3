47193ce37dd2d45349f9ef278b5d772fd9339ee0
DavidDonoghue1601 <G00416773@atu.ie>
2025-02-17 13:14:28 +0000

    Lab 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// IMPORTING A COMPONENT AND B COMPONENT
+import { AComponent } from './a/a.component'; 
+import { BComponent } from './b/b.component';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'root-directive',
+  imports: [RouterOutlet, AComponent, BComponent], // IMPORTING A COMPONENT AND B COMPONENT
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-first-app';
+  name: string = "David";
+  age: number = 25;
+  
 }
---
+++ src/app/b/b.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+// IMPORTING A COMPONENT AND B COMPONENT
+@Component({
+  selector: 'app-b',
+  imports: [],
+  templateUrl: './b.component.html',
+  styleUrl: './b.component.css'
+})
+export class BComponent {
+
+}
